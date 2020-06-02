b083178b928184d519a212a7ad6edfb1e8de4440
Dennis Hawran <56675253+AionixX@users.noreply.github.com>
2020-06-02 20:51:04 +0200

    Update GameController.cs

--- Lowpex/Assets/Scripts/Game/GameController.cs
+++ Lowpex/Assets/Scripts/Game/GameController.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    public GameObject heroPrefab;
+    public FixedJoystick movementJoystick;
 
+    private PlayerData playerData;
+    private Camera mainCamera;
+    private void Start()
+    {
+        this.playerData = StateController.LoadActualPlayer();
+        InitHero(playerData);
+    }
+    private void InitHero(PlayerData playerData)
+    {
+
+        GameObject hero = Instantiate(heroPrefab);
+        heroPrefab.transform.position = playerData.position;
+
+        mainCamera = Camera.main;
+        mainCamera.SendMessage("FollowTarget", hero);
+
+        hero.AddComponent<CharacterController>();
+        hero.SendMessage("InitPlayer", playerData);
+
+        hero.AddComponent<InputController>();
+        hero.SendMessage("TakeTarget", hero);
+        hero.SendMessage("TakeCamera", mainCamera.gameObject);
+        hero.SendMessage("TakeMovementJoystick", movementJoystick);
+    }
     void Update()
     {
-        
+
     }
 }
