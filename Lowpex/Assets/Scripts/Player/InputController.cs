dc9937ae759da361b3f199e875bb887dbf9f2ae1
Dennis Hawran <56675253+AionixX@users.noreply.github.com>
2020-06-02 20:42:21 +0200

    Revert "Revert "improved camera and movement""
    This reverts commit a3d316ba4dcba50d2c59f1e0607906d514341f1d.

---
+++ Lowpex/Assets/Scripts/Player/InputController.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MovementController))]
+public class InputController : MonoBehaviour
+{
+    private FixedJoystick movementJoystick;
+
+    private GameObject target;
+    private GameObject camera;
+
+    private float distance = 0;
+    public void TakeMovementJoystick(FixedJoystick movementJoystick)
+    {
+        this.movementJoystick = movementJoystick;
+    }
+    public void TakeCamera(GameObject camera)
+    {
+        this.camera = camera;
+    }
+    public void TakeTarget(GameObject target)
+    {
+        this.target = target;
+    }
+    void Update()
+    {
+        if (!movementJoystick || !target)
+            return;
+
+        if (Input.touchCount > 0 && movementJoystick.Direction.magnitude == 0)
+        {
+            if (Input.touchCount == 2 && movementJoystick.Direction.magnitude == 0)
+            {
+                Debug.Log("here");
+                ZoomCamera(Input.GetTouch(0), Input.GetTouch(1));
+                return;
+            }
+            else
+            {
+                distance = 0;
+            }
+
+
+            if (Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                RotateCamera(Input.GetTouch(0));
+            }
+        }
+        MoveTarget(movementJoystick.Direction.magnitude);
+
+        if (movementJoystick.Direction.magnitude != 0)
+        {
+            RotateTarget(movementJoystick.Direction);
+        }
+
+    }
+    private void ZoomCamera(Touch firstTouch, Touch secondTouch)
+    {
+        if (firstTouch.phase != TouchPhase.Moved && secondTouch.phase != TouchPhase.Moved)
+            return;
+
+        if (distance == 0)
+        {
+            distance = (firstTouch.position - secondTouch.position).magnitude;
+            return;
+        }
+
+        float newDistance = (firstTouch.position - secondTouch.position).magnitude;
+        camera.SendMessage("Zoom", distance - newDistance);
+    }
+    private void RotateCamera(Touch touch)
+    {
+        camera.SendMessage("Move", touch.deltaPosition);
+    }
+    private void RotateTarget(Vector2 direction)
+    {
+        float angle = -(float)Mathf.Atan2(direction.y, direction.x);
+        angle *= Mathf.Rad2Deg;
+        angle += camera.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, angle + 90, 0);
+
+        target.SendMessage("Rotate", rotation);
+    }
+    private void MoveTarget(float speed)
+    {
+        target.SendMessage("Move", speed);
+    }
+}
---
+++ Lowpex/Assets/Scripts/Player/MovementController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class MovementController : MonoBehaviour
+{
+    private Animator animator;
+
+    private float actualSpeed = 0;
+    private float maxSpeed = 15;
+
+    private float damping = 150;
+
+    private bool isAbleToMove = true;
+    private bool isMoving = false;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        animator.SetFloat("Speed", actualSpeed / maxSpeed);
+        transform.localPosition = Vector3.Lerp(transform.position, transform.position + transform.forward, actualSpeed * Time.deltaTime);
+    }
+    public void Move(float speed)
+    {
+        if (speed < 0.1)
+        {
+            actualSpeed = actualSpeed > 0 ? actualSpeed - (damping * Time.fixedDeltaTime) : 0;
+            actualSpeed = actualSpeed < 0 ? 0 : actualSpeed;
+        } 
+        else
+        {
+            actualSpeed = speed * maxSpeed;
+        }
+    }
+    public void Rotate(Quaternion direction)
+    {
+        transform.rotation = direction;
+    }
+}
